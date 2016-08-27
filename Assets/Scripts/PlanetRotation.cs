2f3bde722654098e43c7b6232446dea784961086
moisesjbc <moisesjbc@gmail.com>
2016-08-27 20:11:54 +0100

    Added planet automatic movement.

--- Assets/Scripts/PlanetRotation.cs
+++ Assets/Scripts/PlanetRotation.cs
@@ -12,10 +12,13 @@ public class PlanetRotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		/*
 		if (Input.GetKey (KeyCode.A)) {
 			transform.Rotate (0.0f, 0.0f, rotationSpeed * Time.deltaTime);
 		}else if(Input.GetKey (KeyCode.D)){
 			transform.Rotate (0.0f, 0.0f, -rotationSpeed * Time.deltaTime);
 		}
+		*/
+		transform.Rotate (0.0f, 0.0f, rotationSpeed * Time.deltaTime);
 	}
 }
