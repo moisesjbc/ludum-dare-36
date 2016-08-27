c49dd81a056a8b030d0a595066f232b45f8a0138
moisesjbc <moisesjbc@gmail.com>
2016-08-27 18:36:08 +0100

    Added enemy rotation.

---
+++ Assets/RandomRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour {
+
+	public float rotationSpeed = 50.0f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (rotationSpeed * Time.deltaTime, rotationSpeed * Time.deltaTime, rotationSpeed * Time.deltaTime);
+	}
+}
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -9,12 +9,14 @@ public class EnemyMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		planet = GameObject.Find("Planet");
+		float xSpin = Random.Range(0,360);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 enemyToPlanetDirection = planet.transform.position - transform.position;
 		gameObject.transform.Translate (speed * Vector3.Normalize (enemyToPlanetDirection) * Time.deltaTime);
+		//transform.Rotate (0.0f, 0.0f, 1.0f);
 	}
 
 	void OnTriggerEnter(Collider other) {
