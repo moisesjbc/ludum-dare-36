9ad4174a3187eedec92611f44b7e53b318ff9d13
moisesjbc <moisesjbc@gmail.com>
2016-08-27 20:44:19 +0100

    Added heno shoot (buggy)

---
+++ Assets/Heno.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Heno : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.Normalize (transform.forward);
+		float distance = Vector3.Distance (Vector3.zero, transform.position);
+		if(distance >=50)
+		{
+			Destroy(gameObject);
+		}	
+	}
+}
---
+++ Assets/TowerShooter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerShooter : MonoBehaviour {
+
+	public GameObject towerBullet;
+	float shootCooldown = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("shootCooldown: " + shootCooldown);
+		shootCooldown -= Time.deltaTime;
+		//if (shootCooldown < 0.0f) {
+			GameObject bullet = (GameObject)Instantiate (towerBullet, transform.position, Quaternion.LookRotation (transform.up, Vector3.back));
+			Debug.Log ("bullet.transform.position: " + bullet.transform.position);
+			shootCooldown = 1.0f;
+		//}
+	}
+}
