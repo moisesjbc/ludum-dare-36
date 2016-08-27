e4a34ee8bcd805c1d124aff571fbd9a3703d3ebc
moisesjbc <moisesjbc@gmail.com>
2016-08-27 19:18:36 +0100

    Added planet movement and towers assets.

---
+++ Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetRotation : MonoBehaviour {
+
+	public float rotationSpeed = 50.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (0.0f, 0.0f, rotationSpeed * Time.deltaTime);
+		}else if(Input.GetKey (KeyCode.D)){
+			transform.Rotate (0.0f, 0.0f, -rotationSpeed * Time.deltaTime);
+		}
+	}
+}
