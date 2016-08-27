823e6d9e2609581451e39c82f940eaf7a35009a4
moisesjbc <moisesjbc@gmail.com>
2016-08-27 12:25:03 +0100

    Added spaceship movement.

---
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipMovement : MonoBehaviour {
+
+	public GameObject planet;
+	public float deltaMovement = 1.0f;
+	public float angle = 0.0f;
+	public Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 spaceshipToPlanet = planet.transform.position - transform.position;
+
+		targetPosition = new Vector3(2.0f * Mathf.Cos (angle), 2.0f * Mathf.Sin (angle), 0.0f);
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * 10);
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			angle += deltaMovement * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			angle -= deltaMovement * Time.deltaTime;
+		}
+	}
+}
