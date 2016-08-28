bbc0768347239100c852caed12d511bedde3b8a1
Jocantaro <ricardolopeztoledo@gmail.com>
2016-08-28 15:36:56 +0100

    Va a petar 2

---
+++ Assets/Scripts/UIStory.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class UIStory : MonoBehaviour {
+
+	public Sprite [] dialogSprites;
+	public Image dialog;
+	int dialogIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		dialog.sprite = dialogSprites [0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			if (dialogIndex < dialogSprites.Length){
+			dialogIndex++;
+			dialog.sprite = dialogSprites [dialogIndex];
+			} else {
+				SceneManager.LoadScene ("main");
+
+		}
+	}
+}
+}
